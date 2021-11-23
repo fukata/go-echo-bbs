5a2c8abb948e7d375e9298106a3f39aa93ae8722
fukata <tatsuya.fukata@gmail.com>
2021-11-23 22:26:31 +0900

    first

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(context echo.Context) error {
+		return context.String(http.StatusOK, "Hello, World!")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1323"
+	}
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
+}
